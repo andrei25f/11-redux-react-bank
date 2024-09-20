3d5b2e30aaa55779f80ed2e81a96c56f34aa7e9a
Andrey <andrey25f@gmail.com>
2024-09-20 14:47:30 +0200

    add thunk

--- src/configureStore/store.js
+++ src/configureStore/store.js
@@ -1,11 +1,11 @@
 import { applyMiddleware, legacy_createStore as createStore } from "redux";
 import { accountReducer } from "../reducer/accountReducer";
-import { loggerEnhancer } from "../enhancers/loggerEnhancer";
-import { thunkEnhancer } from "../reducer/thunkEnhancer";
+import { thunk } from "redux-thunk";
+import logger from "redux-logger";
 
 const initialState = {
     balance: 0,
     quote: 'Winter is comeing'
 }
 
-export const store = createStore(accountReducer, initialState, applyMiddleware(thunkEnhancer, loggerEnhancer));
+export const store = createStore(accountReducer, initialState, applyMiddleware(thunk, logger));
